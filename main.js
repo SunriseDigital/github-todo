70a8eb560a3527a9ab4c9331c35a4af71c3879d9
Masamoto Miyata <miyata@sincere-co.com>
2019-04-18 10:22:46 +0900

    時間着きTODOを引用すると計算がおかしくなる問題に対応。

--- main.js
+++ main.js
@@ -46,27 +46,37 @@ function clacRemainingTime(){
     return undefined
   }
 
-  return [...document.querySelectorAll('.task-list-item')]
-    .map(elem => {
-      return {
-        matches: elem.innerText.match(/\[([0-9.]+?)\]/),
-        checked: elem.querySelector('input[type=checkbox]').checked
-      }
-    })
-    .filter(data => data.matches)
-    .map(data => {
-      return {
-        time: parseFloat(data.matches[1], 10),
-        checked: data.checked
-      }
-    })
-    .reduce((sum, data) => {
-      sum.total += data.time
-      if(!data.checked){
-        sum.remaining += data.time
-      }
-      return sum
-    }, {...initial});
+  const result = calcFor([...document.querySelectorAll('.task-list-item')])
+  // blockquoteを引く
+  const blockquote = calcFor([...document.querySelectorAll('blockquote .task-list-item')])
+
+  return {
+    total: result.total - blockquote.total,
+    remaining: result.remaining - blockquote.remaining
+  }
+}
+
+function calcFor(elements){
+  return elements.map(elem => {
+    return {
+      matches: elem.innerText.match(/\[([0-9.]+?)\]/),
+      checked: elem.querySelector('input[type=checkbox]').checked
+    }
+  })
+  .filter(data => data.matches)
+  .map(data => {
+    return {
+      time: parseFloat(data.matches[1], 10),
+      checked: data.checked
+    }
+  })
+  .reduce((sum, data) => {
+    sum.total += data.time
+    if(!data.checked){
+      sum.remaining += data.time
+    }
+    return sum
+  }, {...initial});
 }
 
 function round(val, precision){
